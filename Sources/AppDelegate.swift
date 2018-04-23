a6c4ddf2480a44e93cc122f55bdb4bc10ff2e451
Jeff Kereakoglow <jkereako@users.noreply.github.com>
2018-04-23 16:55:14 -0400

    Initial commit

---
+++ Sources/AppDelegate.swift
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.swift
+//  BarButtonItemTest
+//
+//  Created by Jeff Kereakoglow on 4/23/18.
+//  Copyright © 2018 AlexisDigital. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+final class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+}
+
---
+++ Sources/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BarButtonItemTest
+//
+//  Created by Jeff Kereakoglow on 4/23/18.
+//  Copyright © 2018 AlexisDigital. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
