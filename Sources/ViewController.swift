d00f46c71e05231b3cb6d573d36f8de335d1ba3a
Jeff Kereakoglow <jkereako@users.noreply.github.com>
2018-04-24 09:55:00 -0400

    Add some styling

--- Sources/ViewController.swift
+++ Sources/ViewController.swift
@@ -9,6 +9,11 @@
 import UIKit
 
 final class ViewController: UIViewController {
+    @IBOutlet private weak var toggle: UISwitch!
+
+    private let offColor = UIColor.gray
+    private let onColor = UIColor.green
+
     init() {
         super.init(nibName: "View", bundle: Bundle(for: ViewController.self))
     }
@@ -19,14 +24,51 @@ final class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        // Always start in the off-position
+        toggle.isOn = false
+
+        // Define the color of the navigation bar
+        navigationController?.navigationBar.barTintColor = .purple
+        navigationController?.navigationBar.isTranslucent = true
+
+        let barButtonItem = UIBarButtonItem(
+            title: "Say Hello",
+            style: .plain,
+            target: self, action: #selector(barButtonItemAction(_:))
+        )
+
+        navigationItem.setRightBarButton(barButtonItem, animated: true)
+
+        // Style the bar button item
+        let attributes: [NSAttributedStringKey: Any] = [.foregroundColor: offColor]
+
+        barButtonItem.setTitleTextAttributes(attributes, for: .normal)
     }
 }
 
 // MARK: - Target-actions
 extension ViewController {
     @IBAction func toggleAction(_ sender: UISwitch) {
-        print("Toggled")
+        guard let barButtonItem = navigationItem.rightBarButtonItem else {
+            assertionFailure("Expected a bar button item")
+            return
+        }
+
+        let color: UIColor = sender.isOn ? onColor : offColor
+        let attributes: [NSAttributedStringKey: Any] = [.foregroundColor: color]
+
+        barButtonItem.setTitleTextAttributes(attributes, for: .normal)
+    }
+
+    @IBAction func barButtonItemAction(_ sender: UISwitch) {
+        let alertController = UIAlertController(
+            title: "Hello!", message: "Hello", preferredStyle: .alert
+        )
+
+        alertController.addAction(UIAlertAction(title: "Dismiss", style: .default))
+        
+        present(alertController, animated: true)
     }
 }
 
